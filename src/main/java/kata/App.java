cf439f59fed3e8f3f5dcf53624353f3fc6f832eb
Lars Eckart <lars.eckart@hey.com>
2022-08-01 18:11:23 +0300

    a first test about validating numbers to be odd

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -4,5 +4,10 @@ import java.io.IOException;
 
 class App {
 
-  public static void main(String[] args) throws IOException {}
+  public static void main(String[] args) throws IOException {
+  }
+
+  public static boolean validateOdd(int number) {
+    return false;
+  }
 }
---
+++ src/test/java/kata/ValidateDataTest.java
@@ -0,0 +1,20 @@
+package kata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.larseckart.tcr.CommitOnGreenExtension;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(CommitOnGreenExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ValidateDataTest {
+
+  @Test
+  void odd_numbers_are_odd() {
+    assertThat(App.validateOdd(3)).isTrue();
+  }
+
+}
