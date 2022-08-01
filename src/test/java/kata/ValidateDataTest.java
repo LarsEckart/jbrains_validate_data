6a6cbc8ceded9b4c28338ab37ff713ab555a6da4
Lars Eckart <lars.eckart@hey.com>
2022-08-01 19:17:07 +0300

    t more examples for odd numbers

--- src/test/java/kata/ValidateDataTest.java
+++ src/test/java/kata/ValidateDataTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @ExtendWith(CommitOnGreenExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ValidateDataTest {
 
-  @Test
+  @ParameterizedTest
+  @ValueSource(ints = {3, 5, -1})
   void odd_numbers_are_odd() {
     assertThat(App.validateOdd(3)).isTrue();
   }
